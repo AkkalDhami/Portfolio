742b37f47cc446debd3345103d97cdd97af9e0ca
AkkalDhami <dhamiakkal21@gmail.com>
2025-05-11 18:31:10 +0545

    Update scrolling.js

--- js/scrolling.js
+++ js/scrolling.js
@@ -4,22 +4,6 @@ const clone = originalItems.cloneNode(true);
 clone.id = 'cloned-items';
 sliderTrack.appendChild(clone);
 
-// Pause animation on hover
-const techIcons = document.querySelectorAll('.tech-icon');
-
-// techIcons.forEach(icon => {
-//     icon.addEventListener('mouseenter', () => {
-//         icon.style.transform = 'scale(1.2)';
-//         icon.style.filter = 'grayscale(0%)';
-//     });
-
-//     icon.addEventListener('mouseleave', () => {
-//         icon.style.transform = 'scale(1)';
-//         icon.style.filter = 'grayscale(100%)';
-//     });
-// });
-
-// Reset animation at the end of each cycle
 sliderTrack.addEventListener('animationiteration', () => {
     sliderTrack.style.transform = 'translateX(0)';
     setTimeout(() => {
